4bc453a1af313b11f2c2a80270293b934f448d59
Stefano Donati <stefano.donati@elettronicagf.it>
2016-07-21 10:46:16 +0200

    Modificata versione in 0533-001alpha Aggiunti script di compilazione

--- compile_mfg.sh
+++ compile_mfg.sh
@@ -1,6 +1,5 @@
 VERSION=$(cat Makefile | grep EXTRAV | grep 0533 | awk -F"-" '{print $2"-"$3}')
 make distclean
-make CROSS_COMPILE=arm-egf-linux-gnueabi- egf_evb_mx6_spl_config
+make CROSS_COMPILE=arm-egf-linux-gnueabi- egf_evb_mx6_mfg_wid0510ab0101_config
 make CROSS_COMPILE=arm-egf-linux-gnueabi- -j8
-cp SPL binaries/SPL-$VERSION
-cp u-boot.img binaries/u-boot.img-$VERSION
+mv u-boot.imx binaries/u-boot.imx.wid0510ab0101-$VERSION
