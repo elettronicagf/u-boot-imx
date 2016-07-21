3886ec872c98e31d8fec092970ba730bfb86cddc
Stefano Donati <stefano.donati@elettronicagf.it>
2016-07-21 10:50:51 +0200

    Impostata UART di debug su UART2

--- include/configs/egf_evb_mx6.h
+++ include/configs/egf_evb_mx6.h
@@ -19,8 +19,8 @@
 #endif
 
 #define CONFIG_MACH_TYPE	3980
-#define CONFIG_MXC_UART_BASE	UART1_BASE
-#define CONFIG_CONSOLE_DEV		"ttymxc0"
+#define CONFIG_MXC_UART_BASE	UART2_BASE
+#define CONFIG_CONSOLE_DEV		"ttymxc1"
 #define CONFIG_MMCROOT			"/dev/mmcblk2p2"  /* SDHC3 */
 #if defined(CONFIG_MX6QP)
 #define CONFIG_DEFAULT_FDT_FILE	"imx6qp-sabresd.dtb"
