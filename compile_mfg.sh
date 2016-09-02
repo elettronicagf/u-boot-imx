07e4904f62cac9e64183fbb37a05d52f4accc61e
Stefano Donati <stefano.donati@elettronicagf.it>
2016-09-02 08:53:45 +0200

    Aggiornato script di compilazione u-boot mfg

--- compile_mfg.sh
+++ compile_mfg.sh
@@ -1,5 +1,11 @@
 VERSION=$(cat Makefile | grep EXTRAV | grep 0533 | awk -F"-" '{print $2"-"$3}')
-make distclean
+#make distclean
+make CROSS_COMPILE=arm-egf-linux-gnueabi- egf_evb_mx6_mfg_wid0510aa0101_config
+make CROSS_COMPILE=arm-egf-linux-gnueabi- -j8
+mv u-boot.imx binaries/u-boot.imx.wid0510aa0101-$VERSION
 make CROSS_COMPILE=arm-egf-linux-gnueabi- egf_evb_mx6_mfg_wid0510ab0101_config
 make CROSS_COMPILE=arm-egf-linux-gnueabi- -j8
 mv u-boot.imx binaries/u-boot.imx.wid0510ab0101-$VERSION
+make CROSS_COMPILE=arm-egf-linux-gnueabi- egf_evb_mx6_mfg_wid0510ac0101_config
+make CROSS_COMPILE=arm-egf-linux-gnueabi- -j8
+mv u-boot.imx binaries/u-boot.imx.wid0510ac0101-$VERSION
