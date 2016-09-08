3eab8618d4f61eeef2020361e74e43445bdbc995
Stefano Donati <stefano.donati@elettronicagf.it>
2016-09-08 10:14:44 +0200

    Added cfg with DDR settings for WID0500_AA01.01 modules Updated compile scripts

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
-VERSION=$(cat Makefile | grep EXTRAV | grep 0533 | awk -F"-" '{print $2"-"$3}')
+VERSION=$(cat Makefile | grep EXTRAV | grep 0541 | awk -F"-" '{print $2"-"$3}')
 make distclean
 make CROSS_COMPILE=arm-egf-linux-gnueabi- egf_evb_mx6_spl_config
 make CROSS_COMPILE=arm-egf-linux-gnueabi- -j8
