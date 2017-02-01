f0d17618af8411a94727a5869383da3363321761
Stefano Donati <stefano.donati@elettronicagf.it>
2017-02-01 15:06:42 +0100

    Added support for WID0500_AB01.01 modules (Quad)

--- compile_mfg.sh
+++ compile_mfg.sh
@@ -3,4 +3,7 @@ VERSION=$(cat Makefile | grep EXTRAV | grep 0541 | awk -F"-" '{print $2"-"$3}')
 make CROSS_COMPILE=arm-egf-linux-gnueabi- egf_evb_mx6_mfg_wid0500aa0101_config
 make CROSS_COMPILE=arm-egf-linux-gnueabi- -j8
 mv u-boot.imx binaries/u-boot.imx.wid0500aa0101-$VERSION
+make CROSS_COMPILE=arm-egf-linux-gnueabi- egf_evb_mx6_mfg_wid0500ab0101_config
+make CROSS_COMPILE=arm-egf-linux-gnueabi- -j8
+mv u-boot.imx binaries/u-boot.imx.wid0500ab0101-$VERSION
 
