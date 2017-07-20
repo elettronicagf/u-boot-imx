f146af489513157514e5faca7f09ba9c0d567f38
Mario Machine <mario.machine@elettronicagf.it>
2017-07-20 08:46:36 +0200

    Modificati script di compilazione
    Signed-off-by: Mario Machine <mario.machine@elettronicagf.it>

--- compile.sh
+++ compile.sh
@@ -1,9 +1,10 @@
 VERSION=$(cat Makefile | grep EXTRAV | grep 0533 | awk -F"-" '{print $2"-"$3}')
 make distclean
+export CROSS_COMPILE=/opt/0533_panel_pc/sdk/toolchain/1.0/sysroots/x86_64-egf-linux/usr/bin/arm-egf-linux-gnueabi/arm-egf-linux-gnueabi-
 export KCFLAGS=--sysroot=/opt/0533_panel_pc/sdk/toolchain/1.0/sysroots/cortexa9hf-vfp-neon-egf-linux-gnueabi
 export KCPPFLAGS=
 export KAFLAGS=
-make CROSS_COMPILE=arm-egf-linux-gnueabi- egf_evb_mx6_spl_config
-make CROSS_COMPILE=arm-egf-linux-gnueabi- -j8
+make egf_evb_mx6_spl_config
+make -j8
 cp SPL binaries/SPL-$VERSION
 cp u-boot.img binaries/u-boot.img-$VERSION
--- compile_mfg.sh
+++ compile_mfg.sh
@@ -1,27 +1,33 @@
 VERSION=$(cat Makefile | grep EXTRAV | grep 0533 | awk -F"-" '{print $2"-"$3}')
 make distclean
+export CROSS_COMPILE=/opt/0533_panel_pc/sdk/toolchain/1.0/sysroots/x86_64-egf-linux/usr/bin/arm-egf-linux-gnueabi/arm-egf-linux-gnueabi-
 export KCFLAGS=--sysroot=/opt/0533_panel_pc/sdk/toolchain/1.0/sysroots/cortexa9hf-vfp-neon-egf-linux-gnueabi
 export KCPPFLAGS=
 export KAFLAGS=
-make CROSS_COMPILE=arm-egf-linux-gnueabi- egf_evb_mx6_mfg_wid0510aa0101_config
-make CROSS_COMPILE=arm-egf-linux-gnueabi- -j8
+make egf_evb_mx6_mfg_wid0510aa0101_config
+make -j8
 mv u-boot.imx binaries/u-boot.imx.wid0510aa0101-$VERSION
-make CROSS_COMPILE=arm-egf-linux-gnueabi- egf_evb_mx6_mfg_wid0510ab0101_config
-make CROSS_COMPILE=arm-egf-linux-gnueabi- -j8
+make egf_evb_mx6_mfg_wid0510ab0101_config
+make -j8
 mv u-boot.imx binaries/u-boot.imx.wid0510ab0101-$VERSION
-make CROSS_COMPILE=arm-egf-linux-gnueabi- egf_evb_mx6_mfg_wid0510ac0101_config
-make CROSS_COMPILE=arm-egf-linux-gnueabi- -j8
+make egf_evb_mx6_mfg_wid0510ac0101_config
+make -j8
 mv u-boot.imx binaries/u-boot.imx.wid0510ac0101-$VERSION
-make CROSS_COMPILE=arm-egf-linux-gnueabi- egf_evb_mx6_mfg_wid0510ae0101_config
-make CROSS_COMPILE=arm-egf-linux-gnueabi- -j8
+make egf_evb_mx6_mfg_wid0510ae0101_config
+make -j8
 mv u-boot.imx binaries/u-boot.imx.wid0510ae0101-$VERSION
-make CROSS_COMPILE=arm-egf-linux-gnueabi- egf_evb_mx6_mfg_wid0510ad0101_config
-make CROSS_COMPILE=arm-egf-linux-gnueabi- -j8
+make egf_evb_mx6_mfg_wid0510ad0101_config
+make -j8
 mv u-boot.imx binaries/u-boot.imx.wid0510ad0101-$VERSION
-make CROSS_COMPILE=arm-egf-linux-gnueabi- egf_evb_mx6_mfg_wid0510af0101_config
-make CROSS_COMPILE=arm-egf-linux-gnueabi- -j8
+make egf_evb_mx6_mfg_wid0510af0101_config
+make -j8
 mv u-boot.imx binaries/u-boot.imx.wid0510af0101-$VERSION
-make CROSS_COMPILE=arm-egf-linux-gnueabi- egf_evb_mx6_mfg_wid0510ag0101_config
-make CROSS_COMPILE=arm-egf-linux-gnueabi- -j8
+make egf_evb_mx6_mfg_wid0510ag0101_config
+make -j8
 mv u-boot.imx binaries/u-boot.imx.wid0510ag0101-$VERSION
-
+make egf_evb_mx6_mfg_wid0510aj0101_config
+make -j8
+mv u-boot.imx binaries/u-boot.imx.wid0510aj0101-$VERSION
+make egf_evb_mx6_mfg_wid0510ak0101_config
+make -j8
+mv u-boot.imx binaries/u-boot.imx.wid0510ak0101-$VERSION
