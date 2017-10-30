d5b06af5d06fda510c26b117164d6fc74526a6d8
Stefano Donati <stefano.donati@elettronicagf.it>
2017-10-30 11:08:37 +0100

    Updated compile scripts for 0556 toolchain

--- compile.sh
+++ compile.sh
@@ -1,6 +1,10 @@
 VERSION=$(cat Makefile | grep EXTRAV | grep 0556 | awk -F"-" '{print $2"-"$3}')
+export CROSS_COMPILE=/opt/0556_evb_pop/sysroots/x86_64-pokysdk-linux/usr/bin/arm-poky-linux-gnueabi/arm-poky-linux-gnueabi-
+export KCFLAGS=--sysroot=/opt/0556_evb_pop/sysroots/cortexa9hf-vfp-neon-poky-linux-gnueabi
+export KCPPFLAGS=
+export KAFLAGS=
 make distclean
-make CROSS_COMPILE=arm-egf-linux-gnueabi- egf_evb_mx6_spl_config
-make CROSS_COMPILE=arm-egf-linux-gnueabi- -j8
+make egf_evb_mx6_spl_config
+make -j8
 cp SPL binaries/SPL-$VERSION
 cp u-boot.img binaries/u-boot.img-$VERSION
--- compile_mfg.sh
+++ compile_mfg.sh
@@ -1,11 +1,15 @@
 VERSION=$(cat Makefile | grep EXTRAV | grep 0556 | awk -F"-" '{print $2"-"$3}')
-#make distclean
-make CROSS_COMPILE=arm-egf-linux-gnueabi- egf_evb_mx6_mfg_wid0500aa0101_config
-make CROSS_COMPILE=arm-egf-linux-gnueabi- -j8
+export CROSS_COMPILE=/opt/0556_evb_pop/sysroots/x86_64-pokysdk-linux/usr/bin/arm-poky-linux-gnueabi/arm-poky-linux-gnueabi-
+export KCFLAGS=--sysroot=/opt/0556_evb_pop/sysroots/cortexa9hf-vfp-neon-poky-linux-gnueabi
+export KCPPFLAGS=
+export KAFLAGS=
+make distclean
+make egf_evb_mx6_mfg_wid0500aa0101_config
+make -j8
 mv u-boot.imx binaries/u-boot.imx.wid0500aa0101-$VERSION
-make CROSS_COMPILE=arm-egf-linux-gnueabi- egf_evb_mx6_mfg_wid0500ab0101_config
-make CROSS_COMPILE=arm-egf-linux-gnueabi- -j8
+make egf_evb_mx6_mfg_wid0500ab0101_config
+make -j8
 mv u-boot.imx binaries/u-boot.imx.wid0500ab0101-$VERSION
-make CROSS_COMPILE=arm-egf-linux-gnueabi- egf_evb_mx6_mfg_wid0500ac0101_config
-make CROSS_COMPILE=arm-egf-linux-gnueabi- -j8
+make egf_evb_mx6_mfg_wid0500ac0101_config
+make -j8
 mv u-boot.imx binaries/u-boot.imx.wid0500ac0101-$VERSION
