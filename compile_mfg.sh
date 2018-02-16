071f618a4146b73bd043ee50efccba77c971c89b
Gianluca Cornacchia <c.luca74@gmail.com>
2018-02-16 17:51:57 +0100

    fix compile_mfg.sh to support standard toolchain

--- compile_mfg.sh
+++ compile_mfg.sh
@@ -1,7 +1,7 @@
 VERSION=$(cat Makefile | grep EXTRAV | grep 0533 | awk -F"-" '{print $2"-"$3}')
 make distclean
-export CROSS_COMPILE=/opt/0533_panel_pc/sdk/toolchain/1.0/sysroots/x86_64-egf-linux/usr/bin/arm-egf-linux-gnueabi/arm-egf-linux-gnueabi-
-export KCFLAGS=--sysroot=/opt/0533_panel_pc/sdk/toolchain/1.0/sysroots/cortexa9hf-vfp-neon-egf-linux-gnueabi
+export CROSS_COMPILE=/opt/fsl-imx-x11/4.1.15-1.2.0/sysroots/x86_64-pokysdk-linux/usr/bin/arm-poky-linux-gnueabi/arm-poky-linux-gnueabi-
+export KCFLAGS=--sysroot=/opt/fsl-imx-x11/4.1.15-1.2.0/sysroots/cortexa9hf-vfp-neon-poky-linux-gnueabi
 export KCPPFLAGS=
 export KAFLAGS=
 make egf_evb_mx6_mfg_wid0510aa0101_config
