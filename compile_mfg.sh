964f6ce05b336098d61651b21e8630bc8ccc899c
Stefano Donati <stefano.donati@elettronicagf.it>
2018-05-14 11:36:23 +0200

    Added support for WID0500_AD01.01 and WID0510_AE01.01 SOM modules

--- compile_mfg.sh
+++ compile_mfg.sh
@@ -9,3 +9,9 @@ mv u-boot.imx binaries/u-boot.imx.wid0500ab0101-$VERSION
 make CROSS_COMPILE=arm-egf-linux-gnueabi- egf_evb_mx6_mfg_wid0500ac0101_config
 make CROSS_COMPILE=arm-egf-linux-gnueabi- -j8
 mv u-boot.imx binaries/u-boot.imx.wid0500ac0101-$VERSION
+make CROSS_COMPILE=arm-egf-linux-gnueabi- egf_evb_mx6_mfg_wid0500ad0101_config
+make CROSS_COMPILE=arm-egf-linux-gnueabi- -j8
+mv u-boot.imx binaries/u-boot.imx.wid0500ad0101-$VERSION
+make CROSS_COMPILE=arm-egf-linux-gnueabi- egf_evb_mx6_mfg_wid0500ae0101_config
+make CROSS_COMPILE=arm-egf-linux-gnueabi- -j8
+mv u-boot.imx binaries/u-boot.imx.wid0500ae0101-$VERSION
